80b1f23a00441f5510f371d72ee0d9a62140cea0
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-04-15 11:35:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  set,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
   databaseURL: `https://shoppinglist-42646-default-rtdb.europe-west1.firebasedatabase.app/`,
 };
 
 const app = initializeApp(appSettings);
+const db = getDatabase(app);
+const shopppingListInDb = ref(db, "movies");
 
 const inputFieldEl = document.querySelector("#input-field");
 const addButtonEl = document.querySelector("#add-button");
+const listEl = document.querySelector("#list");
 
 addButtonEl.addEventListener("click", () => {
-  let inputValue = inputFieldEl.value;
-  console.log(inputValue);
+  if (inputFieldEl.value === "") {
+    return;
+  }
+  const inputValue = inputFieldEl.value;
+  const record = {
+    title: inputValue,
+    isWatched: false,
+    isDone: false,
+    date: new Date().toISOString(),
+  };
+  push(shopppingListInDb, record);
   inputFieldEl.value = "";
+  console.log("record added:", record);
+  listEl.innerHTML += `<li>${inputValue}</li>`;
 });
