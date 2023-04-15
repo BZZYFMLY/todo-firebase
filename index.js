92191923042591876c3278d5413db4cd3106a6c4
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-04-15 19:05:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ import {
   ref,
   push,
   onValue,
+  remove,
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
   databaseURL: `https://shoppinglist-42646-default-rtdb.europe-west1.firebasedatabase.app/`,
 };
 
+const dbName = "todos";
+
 const app = initializeApp(appSettings);
 const db = getDatabase(app);
-const shopppingListInDb = ref(db, "movies");
+const todoListInDb = ref(db, dbName);
 
 const inputFieldEl = document.querySelector("#input-field");
 const addButtonEl = document.querySelector("#add-button");
@@ -27,15 +30,112 @@ const createRecord = (title) => {
   };
 };
 
-const addToList = (record) => (listEl.innerHTML += `<li>${record.title}</li>`);
-const addToDB = (record) => push(shopppingListInDb, record);
+const createElem = ({
+  tag,
+  id,
+  className,
+  text,
+  src,
+  alt,
+  parent,
+  event,
+  children,
+}) => {
+  const elem = document.createElement(tag);
+  className && className.split(" ").forEach((cn) => elem.classList.add(cn));
+  id && (elem.id = id);
+  text && (elem.textContent = text);
+  if (tag === "img") {
+    src && (elem.src = src);
+    alt && (elem.alt = alt);
+  }
+  if (typeof event === "object") {
+    elem.addEventListener(event.type, event.handler);
+  }
+  const newChildren =
+    !!children && !Array.isArray(children) ? [children] : children;
+
+  newChildren &&
+    newChildren.forEach((child) => elem.appendChild(createElem(child)));
+
+  parent && parent.appendChild(elem);
+  return elem;
+};
+
+const addToList = (record) => {
+  createElem({
+    tag: "li",
+    className: "todo-container",
+    id: record.id,
+    parent: listEl,
+    children: [
+      {
+        tag: "div",
+        className: "info-container",
+        children: [
+          {
+            tag: "h2",
+            text: record.title,
+          },
+          {
+            tag: "p",
+            text: record.date,
+          },
+        ],
+      },
+      {
+        tag: "div",
+        className: "button-container",
+        children: [
+          {
+            tag: "button",
+            className: "btn btn-success check",
+            event: {
+              type: "click",
+              handler: () => {
+                console.log("check", record.id);
+              },
+            },
+            children: {
+              tag: "img",
+              src: "assets/done.png",
+              alt: "check",
+            },
+          },
+          {
+            tag: "button",
+            className: "btn btn-danger delete",
+            event: {
+              type: "click",
+              handler: () => {
+                const elemInDb = ref(db, `${dbName}/${record.id}`);
+                remove(elemInDb);
+                console.log("Deleted element:", record);
+              },
+            },
+            children: {
+              tag: "img",
+              src: "assets/delete.png",
+              alt: "delete",
+            },
+          },
+        ],
+      },
+    ],
+  });
+};
+
+const addToDB = (record) => push(todoListInDb, record);
 const resetInputField = () => (inputFieldEl.value = "");
 const resetList = () => (listEl.innerHTML = "");
 
-onValue(shopppingListInDb, (snapshot) => {
-  let shopppingListArray = Object.values(snapshot.val());
+onValue(todoListInDb, (snapshot) => {
+  snapshot.val() === null && resetList();
+  let shopppingListArray = Object.entries(snapshot.val());
   resetList();
-  shopppingListArray.forEach((shoppingItem) => addToList(shoppingItem));
+  shopppingListArray.forEach(([id, shoppingItem]) =>
+    addToList({id, ...shoppingItem})
+  );
 });
 
 addButtonEl.addEventListener("click", () => {
